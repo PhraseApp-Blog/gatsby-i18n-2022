9565a36d5c2dbdc0e6177dcd51b766f5ec7569b0
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-05-04 14:47:50 +0000

    Adding Phrase ICE

--- plugins/gatsby-theme-i18n-i18next-wrapper/src/wrap-page-element.js
+++ plugins/gatsby-theme-i18n-i18next-wrapper/src/wrap-page-element.js
@@ -1,22 +1,33 @@
 import * as React from "react"
 import i18next from "i18next"
 import { I18nextProvider } from "react-i18next"
+import PhraseInContextEditorPostProcessor from "i18next-phrase-in-context-editor-post-processor"
 
 const wrapPageElement = ({ element, props }, options) => {
   const currentLocale = props.pageContext.locale
 
   const translation = require(`${global.GATSBY_THEME_I18N_I18NEXT_WRAPPER}/${currentLocale}/translation.json`)
 
-  i18next.init({
-    lng: currentLocale,
-    resources: { [currentLocale]: { translation } },
-    fallbackLng: "en",
-    initImmediate: false,
-    debug: process.env.NODE_ENV === "development",
-    interpolation: {
-      escapeValue: false,
-    },
-  })
+  i18next
+    .use(
+      new PhraseInContextEditorPostProcessor({
+        phraseEnabled: true,
+        projectId: process.env.GATSBY_PHRASE_PROJECT_ID,
+        prefix: "{{",
+        suffix: "}}",
+      })
+    )
+    .init({
+      lng: currentLocale,
+      resources: { [currentLocale]: { translation } },
+      fallbackLng: "en",
+      initImmediate: false,
+      debug: process.env.NODE_ENV === "development",
+      interpolation: {
+        escapeValue: false,
+      },
+      postProcess: ["phraseInContextEditor"],
+    })
 
   return <I18nextProvider i18n={i18next}>{element}</I18nextProvider>
 }
