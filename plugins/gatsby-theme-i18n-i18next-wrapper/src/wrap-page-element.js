0deec148dab38b8713afdc6d7d816ba94c258f15
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-05-04 15:51:19 +0000

    Refactoring withPhraseICE option

--- plugins/gatsby-theme-i18n-i18next-wrapper/src/wrap-page-element.js
+++ plugins/gatsby-theme-i18n-i18next-wrapper/src/wrap-page-element.js
@@ -8,36 +8,28 @@ const wrapPageElement = ({ element, props }, options) => {
 
   const translation = require(`${global.GATSBY_THEME_I18N_I18NEXT_WRAPPER}/${currentLocale}/translation.json`)
 
-  let instance = options.withPhraseICE
-    ? i18next.use(
-        new PhraseInContextEditorPostProcessor({
-          phraseEnabled: true,
-          projectId: process.env.GATSBY_PHRASE_PROJECT_ID,
-          prefix: "{{",
-          suffix: "}}",
-        })
-      )
-    : i18next
+  i18next
+    .use(
+      new PhraseInContextEditorPostProcessor({
+        phraseEnabled: options.withPhraseICE,
+        projectId: process.env.GATSBY_PHRASE_PROJECT_ID,
+        prefix: "{{",
+        suffix: "}}",
+      })
+    )
+    .init({
+      lng: currentLocale,
+      resources: { [currentLocale]: { translation } },
+      fallbackLng: "en",
+      initImmediate: false,
+      debug: process.env.NODE_ENV === "development",
+      interpolation: {
+        escapeValue: false,
+      },
+      postProcess: ["phraseInContextEditor"],
+    })
 
-  const postProcess = []
-
-  if (options.withPhraseICE) {
-    postProcess.push("phraseInContextEditor")
-  }
-
-  instance.init({
-    lng: currentLocale,
-    resources: { [currentLocale]: { translation } },
-    fallbackLng: "en",
-    initImmediate: false,
-    debug: process.env.NODE_ENV === "development",
-    interpolation: {
-      escapeValue: false,
-    },
-    postProcess,
-  })
-
-  return <I18nextProvider i18n={instance}>{element}</I18nextProvider>
+  return <I18nextProvider i18n={i18next}>{element}</I18nextProvider>
 }
 
 export { wrapPageElement }
